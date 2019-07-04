c177a691038a9e197c709c7dc7a82016a1a546d8
PhoenixDeveloper <mefisto40000x@gmail.com>
2019-07-04 11:48:54 +0300

    Выполнен первый пункт первого задания

---
+++ BMO.GameDevUnity.CSharp2.Pract1/Workers/FixedPayWorker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workers
+{
+    class FixedPayWorker : Worker
+    {
+        public override double WagesPerMonth()
+        {
+            return Math.Round(Salary, 2);
+        }
+    }
+}
---
+++ BMO.GameDevUnity.CSharp2.Pract1/Workers/Worker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workers
+{
+    abstract class Worker
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        private double salary;
+        public double Salary
+        {
+            get
+            {
+                return Math.Round(salary, 2);
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    salary = -Math.Round(value, 2);
+                }
+                else
+                {
+                    salary = Math.Round(value, 2);
+                }
+            }
+        }
+
+        abstract public double WagesPerMonth();
+    }
+}
