884776ac0c35d753d263b0723bab12602802bb97
PhoenixDeveloper <mefisto40000x@gmail.com>
2019-07-16 09:35:55 +0300

    Выполнено нахождение сколько встречается каждый элемент из списка Integer

---
+++ BMO.GameDevUnity.CSharp2.Pract1/CountElementsInList/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountElementsInList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listInteger = new List<int>()
+            {
+                5,
+                6,
+                2,
+                5,
+                3,
+                2
+            };
+            foreach (var element in countElementInListInteger(listInteger))
+            {
+                Console.WriteLine($"Число {element.Key} встречается в списке {element.Value} раз");
+            }
+            Console.ReadKey();
+        }
+
+        static Dictionary<int, int> countElementInListInteger(List<int> list)
+        {
+            list.Sort();
+            Dictionary<int, int> count = new Dictionary<int, int>();
+            foreach (var element in list)
+            {
+                if (!count.ContainsKey(element))
+                {
+                    count.Add(element, 0);
+                }
+                count[element]++;
+            }
+            return count;
+        }
+    }
+}
